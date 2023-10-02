b21bfa7b6e85145f8c24a7faf96cb8e34b840867
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-10-02 11:41:35 +0200

    first commit

---
+++ src/stories/MyButton.stories.js
@@ -0,0 +1,39 @@
+import MyButton from '../components/elements/MyButton.vue';
+export default{
+    title: 'Elements/MyButton',
+    component: MyButton,
+    argTypes: {
+        content: {
+            control: 'text',
+        },
+        href: {
+            control: 'text',
+        
+        },
+        size:{
+            control:'select',
+            options:['small','regular']
+        },
+        variant:{
+            control:'select',
+            options:['default','rounded']
+        },
+    },
+}
+export const PrimaryButton ={
+    render: (args) => {
+        return{
+            components: {MyButton},
+            setup() {
+                return {args};
+            },
+            template: '<MyButton v-bind="args"></MyButton>',
+        }
+    },
+    args: {
+        size: 'small',
+        variant: 'rounded',
+        href: 'about',
+        content: 'Primary',
+    }
+};
