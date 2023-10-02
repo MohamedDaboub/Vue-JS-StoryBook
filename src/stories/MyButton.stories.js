395fac7583410edeb1590dd71339bfceb8f30544
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-10-02 13:38:09 +0200

    fin button

--- src/stories/MyButton.stories.js
+++ src/stories/MyButton.stories.js
@@ -31,6 +31,57 @@ export const PrimaryButton ={
         }
     },
     args: {
+        size: 'regular',
+        variant: 'default',
+        href: 'about',
+        content: 'Primary',
+    }
+};
+export const SecondaryButton ={
+    render: (args) => {
+        return{
+            components: {MyButton},
+            setup() {
+                return {args};
+            },
+            template: '<MyButton v-bind="args"></MyButton>',
+        }
+    },
+    args: {
+        size: 'regular',
+        variant: 'rounded',
+        href: 'about',
+        content: 'Primary',
+    }
+};
+export const SmallPrimaryButton ={
+    render: (args) => {
+        return{
+            components: {MyButton},
+            setup() {
+                return {args};
+            },
+            template: '<MyButton v-bind="args"></MyButton>',
+        }
+    },
+    args: {
+        size: 'small',
+        variant: 'default',
+        href: 'about',
+        content: 'Primary',
+    }
+};
+export const SmallSecondaryButton ={
+    render: (args) => {
+        return{
+            components: {MyButton},
+            setup() {
+                return {args};
+            },
+            template: '<MyButton v-bind="args"></MyButton>',
+        }
+    },
+    args: {
         size: 'small',
         variant: 'rounded',
         href: 'about',
