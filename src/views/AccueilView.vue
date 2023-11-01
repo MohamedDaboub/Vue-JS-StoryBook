bffea73cf2a660185002149b6525f3ac21c99c07
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-11-01 14:45:46 +0100

    Ajoute un var dans le Var scss + termin le footer

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,65 +1,12 @@
 <script setup>
 import Myheader from '@/components/layout/MyHeader.vue'
+import Myfooter from '@/components/layout/MyFooter.vue'
 </script>
 
 <template>
   <Myheader/>
+  <Myfooter/>
 </template>
 
-<style lang="scss" scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: rem(20) rem(50);
-  background-color: white;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  .logo img {
-      height: rem(50);
-      width: auto;
-  }
-.menu {
-    ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        display: flex;
-        gap: rem(15);
-        li {
-            a {
-                text-decoration: none;
-                color: black;
-                &:hover {
-                    text-decoration: underline;
-                }
-            }
-        }
-    }
-}
-
-.right-side {
-    display: flex;
-    align-items: center;
-    gap: rem(10);  
-    border-radius: rem(10);
-    box-shadow: 0px rem(2) rem(10) rgba(0, 0, 0, 0.20);
-    padding: rem(5);
-    
-    input[type="search"] {
-        border: none;
-        outline: none;
-    }
-    &--Search{
-        background-color: white;
-        display: flex;
-        align-items: center;
-        border-radius: rem(15);
-    }
-    &--border{
-        border-left: 1px solid $gray;
-        border-radius: 0px 10px 10px 0px;
-    }
-    }
-}
-  
+<style lang="scss" scoped>  
 </style>
